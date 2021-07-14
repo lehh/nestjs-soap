d67a597e96511c4ad9cc50cc6aaab54c3ea8284a
Robert-Jan <robert-jan.kuyper@vodafoneziggo.com>
2021-07-14 14:18:07 +0200

    test: add jest with code cov

---
+++ jest.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  verbose: true,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  collectCoverage: false,
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.ts'
+  ],
+  collectCoverageFrom: [
+    '**/*.{ts,js}',
+    '!**/{dist,lib,coverage,node_modules}/**',
+    '!*.config.{ts,js}',
+    '!*.d.ts',
+    '!.eslintrc.js',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'gql'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleDirectories: ['node_modules'],
+  transformIgnorePatterns: [],
+  modulePathIgnorePatterns: ['bin', 'dist', 'lib'],
+  globals: {},
+};
