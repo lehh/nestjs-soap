9b7e1fc54af13793ed24be22629ebeaaf1f31aca
Robert-Jan <robert-jan.kuyper@vodafoneziggo.com>
2021-07-14 14:56:13 +0200

    refactor: promise to async await

--- src/soap-utils.ts
+++ src/soap-utils.ts
@@ -1,16 +1,20 @@
+import { ServiceUnavailableException } from '@nestjs/common';
 import { BasicAuthSecurity, Client, createClientAsync, ISecurity } from 'soap';
 import { SoapModuleOptions } from './soap-module-options.type';
 
-export default function createSoapClient(options: SoapModuleOptions): Promise<Client> {
-  return createClientAsync(options.uri, options.clientOptions).then(Client => {
-    if (options.auth) {
-      const basicAuth: ISecurity = new BasicAuthSecurity(
-        options.auth.username,
-        options.auth.password,
-      );
-      Client.setSecurity(basicAuth);
-    }
+export default async function createSoapClient(options: SoapModuleOptions): Promise<Client> {
+  const client = await createClientAsync(options.uri, options.clientOptions)?.catch(err => { 
+    throw new ServiceUnavailableException(err)
+  })
+  
+  if (options.auth) {
+    const basicAuth: ISecurity = new BasicAuthSecurity(
+      options.auth.username,
+      options.auth.password,
+    );
 
-    return Client;
-  });
+    client.setSecurity(basicAuth);
+  }
+
+  return client
 }
