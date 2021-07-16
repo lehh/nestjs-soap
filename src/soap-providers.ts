13ca15574078011bb631fa7eb48494aeb1829508
Robert-Jan <robert-jan.kuyper@vodafoneziggo.com>
2021-07-16 11:33:37 +0200

    fix: connection per name using the nest approach

--- src/soap-providers.ts
+++ src/soap-providers.ts
@@ -1,39 +1,35 @@
 import { FactoryProvider, Provider, Type } from '@nestjs/common';
-import { createClientAsync, Client } from 'soap';
+import { Client } from 'soap';
 import { SOAP_MODULE_OPTIONS } from './soap-constants';
 import {
   SoapModuleAsyncOptions,
   SoapModuleOptions,
   SoapModuleOptionsFactory,
 } from './soap-module-options.type';
+import { createSoapClient } from './soap-utils'
 
-export const buildProvider = (soapOption: SoapModuleOptions): FactoryProvider => ({
-  provide: soapOption.name,
-  useFactory: async (): Promise<Client> => {
-    return await createClientAsync(soapOption.uri, soapOption.clientOptions);
-  },
-});
+export const createProviders = (options: SoapModuleOptions[]): FactoryProvider[] => {
+  return options.map(option => ({
+    provide: option.connectionName,
+    useFactory: async (): Promise<Client> => createSoapClient(option),
+  }));
+}
 
-export const buildProvidersAsync = (soapOptions: SoapModuleOptions[]): FactoryProvider[] =>
-  soapOptions.map(buildProvider);
-
-export const createAsyncProviders = (options: SoapModuleAsyncOptions[]): Provider[] => {
-  return options.map(option => {
-    if (option.useExisting || option.useFactory) {
-      return createAsyncOptionsProvider(option);
-    }
-
-    const useClass = option.useClass as Type<SoapModuleOptionsFactory>;
-
-    return {
-      ...createAsyncOptionsProvider(option),
+export const createAsyncProviders = (options: SoapModuleAsyncOptions): Provider[] => {
+  if (options.useExisting || options.useFactory) {
+    return [this.createAsyncOptionsProvider(options)];
+  }
+  const useClass = options.useClass as Type<SoapModuleOptionsFactory>;
+  return [
+    _createAsyncOptionsProvider(options),
+    {
       provide: useClass,
       useClass,
-    };
-  });
+    },
+  ];
 };
 
-export const createAsyncOptionsProvider = (options: SoapModuleAsyncOptions): Provider => {
+const _createAsyncOptionsProvider = (options: SoapModuleAsyncOptions): Provider => {
   if (options.useFactory) {
     return {
       inject: options.inject || [],
