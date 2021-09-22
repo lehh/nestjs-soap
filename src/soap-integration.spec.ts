c216203f700ff70703bf352b425a092bb0d5faf4
Alessandra <aleterre10@gmail.com>
2021-09-22 17:14:47 -0300

    Fixed integration tests (rjpkuyper fix)

--- src/soap-integration.spec.ts
+++ src/soap-integration.spec.ts
@@ -1,77 +1,138 @@
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, Injectable } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
-import { SoapModuleOptionsFactory } from './soap-module-options.type';
+import { createClientAsync } from 'soap';
+import { mocked } from 'ts-jest/utils';
+import { SoapModuleOptionsFactory } from './soap-module-options.type'
 import { SoapModule } from './';
 
 describe('SoapModule (integration)', () => {
-  let app: INestApplication;
-
   const MY_SOAP_URI = 'http://my.soap.websl';
 
-  const MY_SOAP_CLIENT_REGISTER = 'MY_SOAP_CLIENT_REGISTER';
-  const MY_SOAP_CLIENT_FOR_ROOT = 'MY_SOAP_CLIENT_FOR_ROOT';
-  const MY_SOAP_CLIENT_FOR_ROOT_ASYNC = 'MY_SOAP_CLIENT_FOR_ROOT_ASYNC';
+  describe("forRoot", () => {
+    const MY_SOAP_CLIENT_REGISTER = 'MY_SOAP_CLIENT_REGISTER';
+    const MY_SOAP_CLIENT_FOR_ROOT = 'MY_SOAP_CLIENT_FOR_ROOT';
+    let app: INestApplication;
 
-  const MY_SOAP_CLIENT_PROVIDER_MOCK = {
-    uri: 'http://my.soap.websl',
-  };
+    beforeAll(async () => {
+      // mock the return value to be able to test security
+      mocked(createClientAsync).mockResolvedValueOnce(
+        // @ts-ignore
+        Promise.resolve({
+          setSecurity: jest.fn(),
+          client: "FOR_ROOT"
+        })
+      ).mockResolvedValue(
+        // @ts-ignore
+        Promise.resolve({
+          setSecurity: jest.fn(),
+          client: "REGISTER_ASYNC"
+        })
+      );
 
-  // use a config service to test the forRootAsync
-  class MySoapClientForRootAsyncConfig implements SoapModuleOptionsFactory {
-    createSoapModuleOptions: () => {
-      uri: 'http://my.soap.websl';
-    };
-  }
-
-  beforeAll(async () => {
-    const moduleFixture = await Test.createTestingModule({
-      imports: [
-        SoapModule.registerAsync([
-          {
-            name: MY_SOAP_CLIENT_REGISTER,
-            uri: MY_SOAP_URI,
-          },
-        ]),
-        SoapModule.forRoot([
-          {
-            name: MY_SOAP_CLIENT_FOR_ROOT,
-            uri: MY_SOAP_URI,
-          },
-        ]),
-        SoapModule.forRootAsync([
-          {
-            name: MY_SOAP_CLIENT_FOR_ROOT_ASYNC,
-            useClass: MySoapClientForRootAsyncConfig,
-          },
-        ]),
-      ],
-    })
-      // prevent invoking create asyncClient from soap
-      .overrideProvider(MY_SOAP_CLIENT_FOR_ROOT)
-      .useValue(MY_SOAP_CLIENT_PROVIDER_MOCK)
-      .overrideProvider(MY_SOAP_CLIENT_FOR_ROOT_ASYNC)
-      .useValue(MY_SOAP_CLIENT_PROVIDER_MOCK)
-      .overrideProvider(MY_SOAP_CLIENT_REGISTER)
-      .useValue(MY_SOAP_CLIENT_PROVIDER_MOCK)
+      const moduleFixture = await Test.createTestingModule({
+        imports: [
+          SoapModule.forRoot([
+            {
+              name: MY_SOAP_CLIENT_FOR_ROOT,
+              uri: MY_SOAP_URI
+            },
+            {
+              name: MY_SOAP_CLIENT_REGISTER,
+              uri: MY_SOAP_URI
+            },
+          ]),
+        ],
+      })
       .compile();
+  
+      app = moduleFixture.createNestApplication();
+      await app.init();
+    });
+  
+    afterAll(async () => {
+      await app.close();
+    });
+  
+    test('should be able to get the soapClient using forRoot', async () => {
+      expect(app.get(MY_SOAP_CLIENT_FOR_ROOT)).toEqual(
+        expect.objectContaining({
+          client: "FOR_ROOT"
+        })
+      )
+    });
+  
+    test('should be able to get the soapClient using registerAsync', async () => {
+      expect(app.get(MY_SOAP_CLIENT_REGISTER)).toEqual(
+        expect.objectContaining({
+          client: "REGISTER_ASYNC"
+        })
+      )
+    });
+  })
 
-    app = moduleFixture.createNestApplication();
-    await app.init();
-  });
+  describe("forRootAsync", () => {
+    let app: INestApplication;
+    
+    // use a config service to test the forRootAsync
+    @Injectable()
+    class MySoapClientForRootAsyncConfig implements SoapModuleOptionsFactory {
+      createSoapModuleOptions () {
+        return {
+          uri: "this.config.uri"
+        }
+      }
+    } 
 
-  afterAll(async () => {
-    await app.close();
-  });
+    beforeAll(async () => {
+      mocked(createClientAsync).mockResolvedValue(
+        // @ts-ignore
+        Promise.resolve({
+          client: "FOR_ROOT_ASYNC"
+        })
+      )
 
-  test('should be able to get the service using forRoot', async () => {
-    expect(app.get(MY_SOAP_CLIENT_FOR_ROOT)).toEqual(MY_SOAP_CLIENT_PROVIDER_MOCK);
-  });
+      const moduleFixture = await Test.createTestingModule({
+        imports: [
+          SoapModule.forRootAsync([
+            {
+              useClass: MySoapClientForRootAsyncConfig,
+              name: "MY_CONNECTION"
+            },
+          ]),
+          SoapModule.forRootAsync([
+            {
+              useFactory: () => ({
+                uri: 'my.wsdl.uri'
+              }),
+              name: "MY_FACTORY_CONNECTION"
+            },
+          ]),
+        ],
+      })
+      .compile();
+  
+      app = moduleFixture.createNestApplication();
+      await app.init();
+    });
+  
+    afterAll(async () => {
+      await app.close();
+    });
+  
+    test('should be able to get the client using forRootAsync useClass', async () => {
+      expect(app.get("MY_CONNECTION")).toEqual(
+        expect.objectContaining({
+          client: "FOR_ROOT_ASYNC"
+        })
+      )
+    });
 
-  test('should be able to get the service using forRootAsync', async () => {
-    expect(app.get(MY_SOAP_CLIENT_FOR_ROOT_ASYNC)).toEqual(MY_SOAP_CLIENT_PROVIDER_MOCK);
-  });
-
-  test('should be able to get the service using registerAsync', async () => {
-    expect(app.get(MY_SOAP_CLIENT_REGISTER)).toEqual(MY_SOAP_CLIENT_PROVIDER_MOCK);
-  });
+    test('should be able to get the client using forRootAsync useFactory', async () => {
+      expect(app.get("MY_FACTORY_CONNECTION")).toEqual(
+        expect.objectContaining({
+          client: "FOR_ROOT_ASYNC"
+        })
+      )
+    });
+  })
 });
